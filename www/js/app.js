55f40c734310f5120fb02f8d7445fef05582aeab
Guilherme Nunes <hnunes.gui@gmail.com>
2014-05-14 16:33:13 -0300

    Native Alert

--- www/js/app.js
+++ www/js/app.js
@@ -12,6 +12,18 @@
     $('.help-btn').on('click', function() {
         alert("Some help here...")
     });
+    document.addEventListener('deviceready', function () {
+    if (navigator.notification) { // Override default HTML alert with native dialog
+        window.alert = function (message) {
+            navigator.notification.alert(
+                message,    // message
+                null,       // callback
+                "Workshop", // title
+                'OK'        // buttonName
+            );
+        };
+    }
+}, false);
 
 
     /* ---------------------------------- Local Functions ---------------------------------- */
